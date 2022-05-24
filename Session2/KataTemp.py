5188a767eee6d9d2665a1982d6d53a5ff7d910c3
Deivy Jr <deivyjunior12@gmail.com>
2022-05-24 13:50:37 -0400

    Session 2 - Coding Done

---
+++ Session2/KataTemp.py
@@ -0,0 +1,221 @@
+# Kata Temperature Excercise
+# Author: Deivy Peña
+# ID: 1099409
+# Date: 5/24/2022
+# Session No. 2
+
+# Class to define scales
+
+from pickletools import read_uint1
+
+
+class TemperatureScale:
+    Kelvin = "K"
+    Celsius = "C"
+    Fahrenheit = "F"
+
+# Main class: Temperature
+
+class Temperature:
+    def __init__(self, value, scale = 'TemperatureScale'):
+        self.value = value
+        self.scale = scale
+
+    # From any defined scale to Kelvin method
+    def toKelvin(self):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round(self.value, 2)
+            object = Temperature(obValue, TemperatureScale.Kelvin)
+            return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value + 273.15, 2)
+            object = Temperature(obValue, TemperatureScale.Kelvin)
+            return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round((self.value - 32) * 5/9 + 273.15, 2)
+            object = Temperature(obValue, TemperatureScale.Kelvin)
+            return object
+
+    # From any defined scale to Fahrenheit method
+    def toFahrenheit(self):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round((self.value - 273.15) * 9/5 + 32, 2)
+            object = Temperature(obValue, TemperatureScale.Fahrenheit)
+            return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round((self.value * 9/5) + 32, 2)
+            object = Temperature(obValue, TemperatureScale.Fahrenheit)
+            return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round(self.value, 2)
+            object = Temperature(obValue, TemperatureScale.Fahrenheit)
+            return object
+
+    # From any defined scale to Celsius method
+    def toCelsius(self):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round(self.value - 273.15, 2)
+            object = Temperature(obValue, TemperatureScale.Celsius)
+            return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value, 2)
+            object = Temperature(obValue, TemperatureScale.Celsius)
+            return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round((self.value - 32) * 5/9, 2)
+            object = Temperature(obValue, TemperatureScale.Celsius)
+            return object
+
+        # ToString method (unifying value and scale)
+        def ToString(self):
+            return '{}{}'.format(self.value, self.scale)
+
+    def ToString(self):
+        return '{}{}'.format(self.value, self.scale)
+
+    # Method to add
+    def add(self, othertemp: 'Temperature'):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round(self.value + othertemp.toKelvin().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Kelvin)
+                return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value + othertemp.toCelsius().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Celsius)
+                return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round(self.value + othertemp.toFahrenheit().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Fahrenheit)
+                return object
+
+    # Method to diff
+    def diff(self, othertemp: 'Temperature'):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round(self.value - othertemp.toKelvin().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Kelvin)
+                return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value - othertemp.toCelsius().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Celsius)
+                return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round(self.value - othertemp.toFahrenheit().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Fahrenheit)
+                return object
+    # Method to multiply
+    def multiply(self, othertemp: 'Temperature'):
+        # If scale is Kelvin
+        if(self.scale == 'K'):
+            obValue = round(self.value * othertemp.toKelvin().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Kelvin)
+                return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value * othertemp.toCelsius().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Celsius)
+                return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round(self.value * othertemp.toFahrenheit().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Fahrenheit)
+                return object
+
+    # Metgod to divde
+    def divdeBy(self, othertemp: 'Temperature'):
+        # If division not defined
+        if(othertemp.value == 0):
+            return None
+        # If scale is Kelvin
+        elif(self.scale == 'K'):
+            obValue = round(self.value / othertemp.toKelvin().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Kelvin)
+                return object
+
+        # If scale is Celsius 
+        elif(self.scale == 'C'):
+            obValue = round(self.value / othertemp.toCelsius().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Celsius)
+                return object
+
+        # If scale is Fahrenheit
+        elif(self.scale == 'F'):
+            obValue = round(self.value / othertemp.toFahrenheit().value, 2)
+            if(obValue == 0):
+                return None
+            else:
+                object = Temperature(obValue, TemperatureScale.Fahrenheit)
+                return object
+
+
+
+
+        
+        
+
+
+        
+
+        
+
+
+
